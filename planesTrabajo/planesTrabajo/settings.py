3fa8f03236d6c901d52dcec5b2f81122148d3cb2
BanryokuxD <sergio2alejandro1h@gmail.com>
2024-04-15 17:19:28 -0500

    Correcion tablasmodel

--- planesTrabajo/planesTrabajo/settings.py
+++ planesTrabajo/planesTrabajo/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+# CSRF_TRUSTED_ORIGINS = ['*']
 
 
 
